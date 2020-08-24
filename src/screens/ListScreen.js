79b17ed05bf57f84ff1dc7ae956451f4b3a93f0d
Greg Johnston <gcjohnston91@gmail.com>
2020-08-23 23:56:47 -0400

    keyExtractor implemented

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -2,16 +2,26 @@ import React from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 
 const ListScreen = () => {
+  // const gabeBois = [
+  //   { name: "Reece", key: "0" },
+  //   { name: "Gabe", key: "1" },
+  //   { name: "Cody", key: "2" },
+  //   { name: "Benny", key: "3" },
+  //   { name: "Ronny", key: "4" }
+  // ];
+
   const gabeBois = [
     { name: "Reece" },
-    { name: "Cody" },
     { name: "Gabe" },
+    { name: "Cody" },
     { name: "Benny" },
+    { name: "Ronny" },
   ];
 
   return (
     <View>
       <FlatList
+        keyExtractor={(gabeBoi) => gabeBoi.name}
         data={gabeBois}
         renderItem={({ item }) => {
           return <Text>{item.name}</Text>;
@@ -29,5 +39,9 @@ const styles = StyleSheet.create({});
 // Requires data and renderItem props
 // This is similar to .map() from React Web
 // renderItem={(element) => {
-// element === {item: {name: 'Reece'}}
+//  element === {item: {name: 'Reece'}}
 // It is common practice to destructure item off of element
+
+// Keys are needed: must be a string and must be unique
+// Keys can be added manually or through a keyExtractor
+// keyExtractor works similarly to key={i} in .map()
