33613dfcbdea61baf8dfe6616195c54f080dc2b5
Greg Johnston <gjohnston@nexient.com>
2020-10-05 10:48:18 -0400

    Square screen refactored with useReducer and Reset Colors button

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,48 +1,56 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useReducer } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
+const initialState = { red: 0, green: 0, blue: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'red':
+      return state.red + action.amount > 255 || state.red + action.amount < 0
+        ? state : { ...state, red: state.red + action.amount };
+    case 'green':
+      return state.green + action.amount > 255 || state.green + action.amount < 0
+        ? state : { ...state, green: state.green + action.amount };
+    case 'blue':
+      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        ? state : { ...state, blue: state.blue + action.amount };
+    case 'reset':
+      return initialState;
+    default:
+      return state;
+  }
+};
+
 const SquareScreen = () => {
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const COLOR_INCREMENT = 15;
 
-  const setColor = (color, change) => {
-    switch (color) {
-      case 'red':
-        red + change > 255 || red + change < 0 ? null : setRed(red => red + change);
-        return;
-      case 'green':
-        green + change > 255 || green + change < 0 ? null : setGreen(green => green + change);
-        return;
-      case 'blue':
-        blue + change > 255 || blue + change < 0 ? null : setBlue(blue => blue + change);
-        return;
-      default:
-        return;
-    }
-  };
+  const { red, green, blue } = state;
 
   return (
     <View>
       <ColorCounter color='Red'
-                    onIncrease={() => setColor('red', COLOR_INCREMENT)}
-                    onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}/>
+                    onIncrease={() => dispatch({ type: 'red', amount: COLOR_INCREMENT })}
+                    onDecrease={() => dispatch({ type: 'red', amount: -COLOR_INCREMENT })} />
       <ColorCounter color='Green'
-                    onIncrease={() => setColor('green', COLOR_INCREMENT)}
-                    onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}/>
+                    onIncrease={() => dispatch({ type: 'green', amount: COLOR_INCREMENT })}
+                    onDecrease={() => dispatch({ type: 'green', amount: -COLOR_INCREMENT })} />
       <ColorCounter color='Blue'
-                    onIncrease={() => setColor('blue', COLOR_INCREMENT)}
-                    onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}/>
+                    onIncrease={() => dispatch({ type: 'blue', amount: COLOR_INCREMENT })}
+                    onDecrease={() => dispatch({ type: 'blue', amount: -COLOR_INCREMENT })} />
+      <View>
+        <Button title={'Reset Colors'}
+                onPress={() => dispatch({ type: 'reset' })} />
+      </View>
       <Text>RGB: {red}, {green}, {blue}</Text>
       <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red},${green},${blue})` }}>
       </View>
     </View>
   );
 };
-//      <View style={{height: 150, width: 150, backgroundColor: `rgb({red},{green},{blue})`}}>
 const styles = StyleSheet.create({});
 
 export default SquareScreen;
