4df7ec8be84e33e17f508dc52d410e26f0fb0041
Greg Johnston <gjohnston@nexient.com>
2020-10-06 11:25:07 -0400

    Styling module done

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,21 +3,37 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>BoxScreen</Text>
+    <View style={styles.parentStyle}>
+      <View style={styles.viewOneStyle}/>
+      <View style={styles.viewTwoStyle}/>
+      <View style={styles.viewThreeStyle}/>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  viewStyle: {
+  parentStyle: {
     borderWidth: 3,
-    borderColor: 'black'
+    borderColor: 'black',
+    height: 100,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
-  textStyle: {
-    borderWidth: 10,
-    borderColor: 'red',
-    margin: 20
+  viewOneStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'red'
+  },
+  viewTwoStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'green',
+    alignSelf: 'flex-end'
+  },
+  viewThreeStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'purple'
   }
 });
 
